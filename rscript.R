d6e57375a12823daa0519a8fe7d7dac066b976a0
Maxwell Scott <gc9922zt@mnstate.edu>
2019-05-02 14:11:06 -0500

    changing

--- rscript.R
+++ rscript.R
@@ -4,9 +4,20 @@ library(tidyverse)
 library(tibble)
 library(readr)
 
+install.packages("ggplot2")
+
 data 
 
-actical_data_2019 <- read_csv("practical-data-2019.csv")
+
+
+
+ggplot2::ggplot(practical_data_2019)
+
+
+
+
+
+practical_data_2019 <- read_csv("practical-data-2019.csv")
 View(practical_data_2019)
 
 print(practical_data_2019)
@@ -21,7 +32,18 @@ read_csv("practical-data-2019.csv")
 
 ggplot("practical-data-2019.csv") +
   geom_histogram(mapping = aes(y = mosq_count, x = County), color='darkred')
-                 
 
-ggplot("practical-data-2019.csv") +
+
+ggplot(data ="practical-data-2019.csv")+
   geom_point(mapping = aes(y = mosq_count, x = County), color='darkred')
+
+
+
+
+
+#calculating standard error Clay
+47.32283/9.433981
+#calculating standard error Norman
+47.88644/12.52996
+#calculating standard error Wilkin
+45.40464/5.477226
